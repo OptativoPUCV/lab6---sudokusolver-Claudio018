8107b4db3172898e374d862bcb32802d17419ad2
Replit user <>
2023-05-04 22:12:14 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -107,7 +107,7 @@ int is_valid(Node* n){
     for (int k = 0; k < 9; k++) {
 
 
-        int sudok[10] = {};
+        int sudok[10] = {0};
 
         for (int p = 0; p < 9; p++) {
             int i = 3 * (k / 3) + (p / 3);
