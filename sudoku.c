08c4d321b528a6d375c35fc603947378cc511ffd
Replit user <>
2023-05-04 17:13:54 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -75,7 +75,7 @@ List* get_adj_nodes(Node* n){
                                 break;
                             } 
                 }
-                if(entro != 1){
+                if(entro == 0){
                     adj->sudo[i][j] = num;
                     pushFront(list,adj);
                 }
