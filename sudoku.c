b4d2e6a788a3797002a2cfa88b6766caff109c47
Replit user <>
2023-05-05 00:23:10 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -134,7 +134,7 @@ List* get_adj_nodes(Node* n){
 
     for(i = 0 ; i < 9 ; i++)
     {
-        for(j = 0 ; j < 9 : j++)
+        for(j = 0 ; j < 9 ; j++)
         
             if(n->sudo[i][k] == 0){
                 entro = 1;
